112c5981939ecc15f63d995d3ba59912326687fa
Yevhen Havrasiienko <evgengavrasienko@gmail.com>
2026-06-25 09:42:15 +0300

    feat: local values

--- frontend/src/lib/graphql-client.ts
+++ frontend/src/lib/graphql-client.ts
@@ -1,8 +1,15 @@
 import { GraphQLClient } from 'graphql-request';
 
 const PROD_GRAPHQL_ENDPOINT = 'https://api.gavrafana.com/graphql';
+const LOCAL_HOSTNAMES = new Set(['localhost', '127.0.0.1', '::1']);
+
+const isLocalhost = () => LOCAL_HOSTNAMES.has(window.location.hostname);
 
 const getEndpoint = () => {
+  if (isLocalhost()) {
+    return `${window.location.origin}/graphql`;
+  }
+
   const envEndpoint = import.meta.env.VITE_GRAPHQL_ENDPOINT;
 
   if (envEndpoint) {
